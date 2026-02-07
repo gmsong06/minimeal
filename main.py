5a74113a609abaa38f94b0158e305239c1c608c0
Ann Song <gmsong1978@gmail.com>
2026-02-06 21:50:49 -0500

    Custom llm prompt

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ load_dotenv()
 
 client = OpenAI()
 
-def ask_llm(prompt: str) -> str:
+def ask_llm(user_prompt: str) -> str:
     response = client.responses.create(
         model="gpt-5.2",
-        input=prompt
+        prompt={
+            "id": "pmpt_6986a5d02164819084be45615e1df3d408d1063967f88be9",
+            "version": "2"
+        },
+        input=user_prompt
     )
     return response.output_text
 
 if __name__ == "__main__":
-    prompt = "What is the capital of France?"
+    prompt = "broccoli cheddar soup wiht buttr chicken and garlic cheese naan and potato pierogis"
     answer = ask_llm(prompt)
     print(f"Answer: {answer}")
